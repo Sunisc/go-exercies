4c16531d5fb9d3bd47b963a935b7406e972f5bfd
Sunisc <surya.poddutoori@gmail.com>
2024-04-19 04:00:24 -0400

    extracting variables from uri path

--- go-web-examples/main.go
+++ go-web-examples/main.go
@@ -6,8 +6,9 @@ import (
 )
 
 func main() {
-	httpServer()
-	helloWorld()
+	router()
+	// httpServer()
+	// helloWorld()
 }
 
 func helloWorld() {
@@ -25,7 +26,24 @@ func httpServer() {
 
 	fs := http.FileServer(http.Dir("./static/"))
 
+	// for files to be served. the url path of the directory needs to be stripped
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.ListenAndServe(":9000", nil)
+}
+
+// Extracting variables from the url path
+func router() {
+	r := http.NewServeMux()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to the library!")
+	})
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		title := r.PathValue("title")
+		page := r.PathValue("page")
+
+		fmt.Fprintf(w, "You've requested the book: %s and page: %s", title, page)
+	})
+	http.ListenAndServe("localhost:6182", r)
 }
